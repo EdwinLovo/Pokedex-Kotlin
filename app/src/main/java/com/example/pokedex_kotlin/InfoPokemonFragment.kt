d0bd2e14368bdcd1a9bcbb0c1d0a02964b9134e5
EdwinLovo <00120617@uca.edu.sv>
2019-04-13 22:56:29 -0600

    Agregando fragmento para mostrar información del pokemon en la misma actividad (MainActivity)

---
+++ app/src/main/java/com/example/pokedex_kotlin/InfoPokemonFragment.kt
@@ -0,0 +1,68 @@
+package com.example.pokedex_kotlin
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_info_pokemon.view.*
+
+private const val ARG_PARAM1 = "nombre"
+
+class InfoPokemonFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private var listener: OnFragmentInteractionListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+        }
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_info_pokemon, container, false).apply {
+            nombreTextViewInfo.text = arguments?.getString("nombre")
+        }
+    }
+
+    fun onButtonPressed(uri: Uri) {
+        listener?.onFragmentInteraction(uri)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String) =
+                InfoPokemonFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                    }
+                }
+    }
+}
